833791c61a1ee39af2ff2f801ea6a90ecff145d3
Viktor Uvarchev <113724227+Viktor-Uv@users.noreply.github.com>
2024-11-24 12:14:04 +0100

    Add update and delete operations

--- controller/profileController.js
+++ controller/profileController.js
@@ -1,7 +1,17 @@
-const {create, read, readAll} = require("../model/profileModel");
+const {
+  create,
+  read,
+  readAll,
+  update,
+  remove
+} = require("../model/profileModel");
 
 export const createProfile = (req, res) => {};
 
 export const readProfile = (req, res) => {};
 
 export const readAllProfiles = (req, res) => {};
+
+export const updateProfile = (req, res) => {};
+
+export const deleteProfile = (req, res) => {};
--- model/profileModel.js
+++ model/profileModel.js
@@ -5,3 +5,7 @@ export const create = (body, cb) => {};
 export const read = (id, cb) => {};
 
 export const readAll = (cb) => {};
+
+export const update = (id, body, cb) => {};
+
+export const remove = (id, cb) => {};
