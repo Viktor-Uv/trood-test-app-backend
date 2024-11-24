6fb149a8c1b06812f94165be358870f81eecece4
Viktor Uvarchev <113724227+Viktor-Uv@users.noreply.github.com>
2024-11-24 15:57:01 +0100

    Refactor profileModel to use promises instead of cb

--- model/profileModel.js
+++ model/profileModel.js
@@ -1,29 +1,30 @@
 const db = require("../firestore");
-const validateProfile = require('../validator/profileValidator');
+const validateProfile = require("../validator/profileValidator");
+const e = require("express");
 const PROFILE_COLLECTION = "profiles";
 
-const create = async (body, cb) => {
-  const { error, profile } = validateProfile(body);
-  if (error) {
-    return cb(error.message);
+const create = async (body) => {
+  try {
+    validateProfile(body);
+  } catch (error) {
+    throw new Error(`Validation Error: ${error.details}`);
   }
 
   try {
-    const docRef = db.collection(PROFILE_COLLECTION).doc();
-    await docRef.set(profile);
-    cb(null, { id: docRef.id, ...profile });
+    const docRef = await db.collection("profiles").add(body);
+    return  { id: docRef.id, ...body };
   } catch (err) {
-    cb(err.message);
+    throw new Error(err.message);
   }
 };
 
-const read = (id, cb) => {};
+const read = (id) => {};
 
-const readAll = (cb) => {};
+const readAll = () => {};
 
-const update = (id, body, cb) => {};
+const update = (id, body) => {};
 
-const remove = (id, cb) => {};
+const remove = (id) => {};
 
 module.exports = {
   create,
