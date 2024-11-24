69522181aa30a042288866dc4889a6c7e6ad0453
Viktor Uvarchev <113724227+Viktor-Uv@users.noreply.github.com>
2024-11-24 22:05:05 +0100

    Use validated object to add missing default fields

--- model/profileModel.js
+++ model/profileModel.js
@@ -8,10 +8,11 @@ const create = async (body) => {
   if (validationResult.error) {
     throw new Error(validationResult.error.message);
   }
+  const validProfile = validationResult.value;
 
   try {
-    const docRef = await db.collection("profiles").add(body);
-    return  { id: docRef.id, ...body };
+    const docRef = await db.collection(PROFILE_COLLECTION).add(validProfile);
+    return  { id: docRef.id, ...validProfile };
   } catch (err) {
     throw new Error(err.message);
   }
