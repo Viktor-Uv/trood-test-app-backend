506c30d9c6b40992c8dd30fd7b36d0c887c12f13
Viktor Uvarchev <113724227+Viktor-Uv@users.noreply.github.com>
2024-11-24 14:49:07 +0100

    Complete profileModel create

--- model/profileModel.js
+++ model/profileModel.js
@@ -1,6 +1,21 @@
-const {db} = require("../firestore");
+const db = require("../firestore");
+const validateProfile = require('../validator/profileValidator');
+const PROFILE_COLLECTION = "profiles";
 
-const create = (body, cb) => {};
+const create = async (body, cb) => {
+  const { error, profile } = validateProfile(body);
+  if (error) {
+    return cb(error.message);
+  }
+
+  try {
+    const docRef = db.collection(PROFILE_COLLECTION).doc();
+    await docRef.set(profile);
+    cb(null, { id: docRef.id, ...profile });
+  } catch (err) {
+    cb(err.message);
+  }
+};
 
 const read = (id, cb) => {};
 
