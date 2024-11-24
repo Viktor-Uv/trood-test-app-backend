0067566eea04c45b4641b31356532a8207a9612d
Viktor Uvarchev <113724227+Viktor-Uv@users.noreply.github.com>
2024-11-24 11:29:15 +0100

    Add profileController.js

---
+++ controller/profileController.js
@@ -0,0 +1,7 @@
+const {create, read, readAll} = require("../model/profileModel");
+
+export const createProfile = (req, res) => {};
+
+export const readProfile = (req, res) => {};
+
+export const readAllProfiles = (req, res) => {};
