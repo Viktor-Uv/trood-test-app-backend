baf61710a09ba4ecda84c9bf237140642e974925
Viktor Uvarchev <113724227+Viktor-Uv@users.noreply.github.com>
2024-11-24 22:02:06 +0100

    Improve error description

--- validator/profileValidator.js
+++ validator/profileValidator.js
@@ -2,37 +2,57 @@ const Joi = require('joi');
 
 const profileSchema = Joi.object({
   name: Joi.string()
-    .pattern(/^[A-Za-zА-Яа-яЁё\s-]+$/)
+    .pattern(
+      /^[A-Za-zА-Яа-яЁё\s-]+$/,
+      {name: "[only letters, spaces & hyphens]"}
+    )
     .min(2)
     .max(50)
     .required(),
 
   surname: Joi.string()
-    .pattern(/^[A-Za-zА-Яа-яЁё\s-]+$/)
+    .pattern(
+      /^[A-Za-zА-Яа-яЁё\s-]+$/,
+      {name: "[letters & spaces]"}
+    )
     .min(2)
     .max(50)
     .required(),
 
   jobTitle: Joi.string()
-    .pattern(/^[A-Za-zА-Яа-яЁё0-9\s.,-]+$/)
+    .pattern(
+      /^[A-Za-zА-Яа-яЁё0-9\s-]+$/,
+      {name: "[letters, numbers, spaces & hyphens]"}
+    )
     .max(100),
 
   phone: Joi.string()
-    .pattern(/^\+\d{10,15}$/)
+    .pattern(
+      /^\+\d{10,15}$/,
+      {name: "[starts with '+' and followed by 10-15 digits]"}
+    )
     .required(),
 
   address: Joi.string()
-    .pattern(/^[A-Za-zА-Яа-яЁё0-9\s.,-]+$/)
+    .pattern(
+      /^[A-Za-zА-Яа-яЁё0-9\s.,-]+$/,
+      {name: "[letters, numbers, spaces, punctuation & hyphens]"}
+    )
     .max(200),
 
   interests: Joi.array()
     .items(
       Joi.string()
+        .pattern(
+          /^[A-Za-zА-Яа-яЁё0-9\s.,]+$/,
+          {name: "[letters, numbers, spaces & punctuation]"}
+        )
         .max(30)
-    ).max(10),
+    )
+    .max(10),
 
-  links: Joi.string()
-    .pattern(/^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w.-]*)*\/?$/)
+  link: Joi.string()
+    .uri()
     .max(200),
 
   profileVisibility: Joi.string()
