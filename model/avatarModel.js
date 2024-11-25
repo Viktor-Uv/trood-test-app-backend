0ffad9fd72e633a2138e1554ada8419fee5864d7
Viktor Uvarchev <113724227+Viktor-Uv@users.noreply.github.com>
2024-11-25 13:26:07 +0100

    Add avatarModel

---
+++ model/avatarModel.js
@@ -0,0 +1,8 @@
+const ValidationError = require("../error/ValidationError");
+const NotFoundError = require("../error/NotFoundError");
+
+const upload = async (file) => {};
+
+const fetch = async (url) => {};
+
+module.exports = { upload, fetch };
