94a9dcb9b03ba83cbdc7db0a5f3c7f2a4cc593f2
Viktor Uvarchev <113724227+Viktor-Uv@users.noreply.github.com>
2024-11-25 20:55:20 +0100

    Generate and return uploaded avatar url

--- controller/avatarController.js
+++ controller/avatarController.js
@@ -1,11 +1,18 @@
 const { upload, fetch } = require("../model/avatarModel");
 const ValidationError = require("../error/ValidationError");
 const NotFoundError = require("../error/NotFoundError");
+const buildFileUrl = require("../util/urlUtility");
 
 const uploadAvatar = async (req, res) => {
   try {
-    const fileUrl = await upload(req.file);
-    res.status(201).json({ url: fileUrl });
+    const uploadedAvatarFileName = await upload(req.file);
+    const avatarUrl = buildFileUrl(
+      req.protocol,
+      req.headers.host,
+      req.path,
+      uploadedAvatarFileName
+    );
+    res.status(201).json({ url: avatarUrl });
   } catch (err) {
     if (err instanceof ValidationError) {
       return res.status(400).json({ error: err.message });
