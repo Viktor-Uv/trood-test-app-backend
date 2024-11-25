07c167d5f1debeef8ed06abfd381e2f2bb08d7e0
Viktor Uvarchev <113724227+Viktor-Uv@users.noreply.github.com>
2024-11-25 20:53:24 +0100

    Add multer middleware to extract file from the request stream

--- route/avatarRouter.js
+++ route/avatarRouter.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const multer = require("multer");
 const router = express.Router();
 const { uploadAvatar, fetchAvatar } = require("../controller/avatarController");
 const AVATAR_PATH = "/avatars";
+const tmpStorage = multer({ storage: multer.memoryStorage() });
 
-router.post(AVATAR_PATH, uploadAvatar);
+router.post(AVATAR_PATH, tmpStorage.single("avatar"), uploadAvatar);
 
 router.get(`${AVATAR_PATH}/:id`, fetchAvatar);
 
