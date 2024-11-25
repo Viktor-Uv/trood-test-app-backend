a33c75f5b92726a4a34be42fb98defb3bcda674c
Viktor Uvarchev <113724227+Viktor-Uv@users.noreply.github.com>
2024-11-25 13:28:32 +0100

    Add avatarController

---
+++ controller/avatarController.js
@@ -0,0 +1,9 @@
+const { upload, fetch } = require("../model/avatarModel");
+const ValidationError = require("../error/ValidationError");
+const NotFoundError = require("../error/NotFoundError");
+
+const uploadAvatar = async (req, res) => {};
+
+const fetchAvatar = async (req, res) => {};
+
+module.exports = { uploadAvatar, fetchAvatar };
