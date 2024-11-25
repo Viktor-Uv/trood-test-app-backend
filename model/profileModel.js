80199a39108eb9d47cc77e12ef03f9dabc14b123
Viktor Uvarchev <113724227+Viktor-Uv@users.noreply.github.com>
2024-11-25 01:30:45 +0100

    Removed redundant try-catch in profileModel

--- model/profileModel.js
+++ model/profileModel.js
@@ -2,56 +2,29 @@ const db = require("../firestore");
 const validateProfile = require("../validator/profileValidator");
 const ValidationError = require("../error/ValidationError");
 const NotFoundError = require("../error/NotFoundError");
-
 const PROFILE_COLLECTION = "profiles";
 
 const create = async (body) => {
-  let validProfile;
-  try {
-    validProfile = validateBody(body);
-  } catch (err) {
-    throw new ValidationError(err.message);
-  }
-
-  try {
-    const docRef = await db.collection(PROFILE_COLLECTION).add(validProfile);
-    return { id: docRef.id, ...validProfile };
-  } catch (err) {
-    throw new Error(err.message);
-  }
+  const validProfile = validateBody(body);
+  const docRef = await db.collection(PROFILE_COLLECTION).add(validProfile);
+  return { id: docRef.id, ...validProfile };
 };
 
 const read = async (id) => {
-  try {
-    const doc = await db.collection(PROFILE_COLLECTION).doc(id).get();
-    if (!doc.exists) {
-      throw new NotFoundError();
-    }
-    return { id: doc.id, ...doc.data() };
-  } catch (err) {
-    if (err instanceof NotFoundError) {
-      throw new NotFoundError(`Id '${id}' not found`);
-    }
-    throw new Error(err.message);
+  const doc = await db.collection(PROFILE_COLLECTION).doc(id).get();
+  if (!doc.exists) {
+    throw new NotFoundError(`Id '${id}' not found`);
   }
+  return { id: doc.id, ...doc.data() };
 };
 
 const readAll = async () => {
-  try {
-    const snapshot = await db.collection(PROFILE_COLLECTION).get();
-    return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-  } catch (err) {
-    throw new Error(err.message);
-  }
+  const snapshot = await db.collection(PROFILE_COLLECTION).get();
+  return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
 };
 
 const update = async (id, body) => {
-  let validProfile;
-  try {
-    validProfile = validateBody(body);
-  } catch (err) {
-    throw new ValidationError(err.message);
-  }
+  const validProfile = validateBody(body);
 
   try {
     await db.collection(PROFILE_COLLECTION).doc(id).update(validProfile);
@@ -65,17 +38,13 @@ const update = async (id, body) => {
 };
 
 const remove = async (id) => {
-  try {
-    await db.collection(PROFILE_COLLECTION).doc(id).delete();
-  } catch (err) {
-    throw new Error(err.message);
-  }
+  await db.collection(PROFILE_COLLECTION).doc(id).delete();
 };
 
 const validateBody = (body) => {
   const validationResult = validateProfile(body);
   if (validationResult.error) {
-    throw new Error(validationResult.error.message);
+    throw new ValidationError(validationResult.error.message);
   }
   return validationResult.value;
 }
