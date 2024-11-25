8bc0f08862dd6cff47018c15c6c9fa5fe7dd8501
Viktor Uvarchev <113724227+Viktor-Uv@users.noreply.github.com>
2024-11-25 15:07:17 +0100

    Add avatarRouter

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const cors = require("cors");
 const port = process.env.PORT || 8000;
 require('dotenv').config();
 const profileRouter = require("./route/profileRouter");
+const avatarRouter = require("./route/avatarRouter");
 
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use("/", profileRouter);
+app.use("/", avatarRouter);
 
 app.listen(port, () => {
   console.log(`The app is listening on port ${port}`);
