8904d92d1cfdf93196cad161ffef842b98ddfb3e
Viktor Uvarchev <113724227+Viktor-Uv@users.noreply.github.com>
2024-11-25 14:58:05 +0100

    Complete avatarController uploadAvatar

--- controller/avatarController.js
+++ controller/avatarController.js
@@ -2,7 +2,17 @@ const { upload, fetch } = require("../model/avatarModel");
 const ValidationError = require("../error/ValidationError");
 const NotFoundError = require("../error/NotFoundError");
 
-const uploadAvatar = async (req, res) => {};
+const uploadAvatar = async (req, res) => {
+  try {
+    const fileUrl = await upload(req.file);
+    res.status(201).json({ url: fileUrl });
+  } catch (err) {
+    if (err instanceof ValidationError) {
+      return res.status(400).json({ error: err.message });
+    }
+    res.status(500).json({ error: err.message });
+  }
+};
 
 const fetchAvatar = async (req, res) => {};
 
