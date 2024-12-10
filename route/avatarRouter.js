af638c240fca1eea5a98cca014703c8b2534a38a
Viktor Uvarchev <113724227+Viktor-Uv@users.noreply.github.com>
2024-12-10 12:26:45 +0100

    Remove multer from avatarRouter

--- route/avatarRouter.js
+++ route/avatarRouter.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const multer = require("multer");
 const router = express.Router();
 const { uploadAvatar, fetchAvatar } = require("../controller/avatarController");
 const AVATAR_PATH = "/avatars";
-const tmpStorage = multer({ storage: multer.memoryStorage() });
 
-router.post(AVATAR_PATH, tmpStorage.single("avatar"), uploadAvatar);
+router.post(AVATAR_PATH, uploadAvatar);
 
 router.get(`${AVATAR_PATH}/:filename`, fetchAvatar);
 
