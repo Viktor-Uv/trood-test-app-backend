2f01ea774e95b170240499aa444a5a8f48ce6868
Viktor Uvarchev <113724227+Viktor-Uv@users.noreply.github.com>
2024-12-10 12:26:12 +0100

    Set up a fileParser that doesn't rely on a form's file field name

---
+++ util/fileParser.js
@@ -0,0 +1,33 @@
+import multer from "multer";
+import ValidationError from "../error/ValidationError.js";
+
+const multerParser = multer({
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+export const parseSingleFile = (req, res) => {
+  const multerMiddleware = multerParser.any();
+
+  return new Promise((resolve, reject) => {
+    multerMiddleware(req, res, (err) => {
+      if (err) {
+        reject(err);
+      }
+
+      const files = req.files;
+
+      if (!files || files.length === 0) {
+        return reject(new ValidationError('File is required'));
+      }
+
+      if (files.length > 1) {
+        return reject(new ValidationError('Single file per request is required'));
+      }
+
+      req.file = files[0];
+
+      resolve();
+    });
+  });
+};
